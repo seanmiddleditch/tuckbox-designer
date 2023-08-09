ea637eee050f74e38d45b74ca91461b1f5445b35
Sean Middleditch <sean.middleditch@gmail.com>
2023-08-09 09:44:59 -0700

    Improved numeric input with validation and control

--- src/components/number-input.tsx
+++ src/components/number-input.tsx
@@ -1,4 +1,4 @@
-import { Component } from 'solid-js'
+import { Component, createSignal } from 'solid-js'
 import { TextField, InputAdornment } from '@suid/material'
 
 interface NumberInputProps {
@@ -11,17 +11,39 @@ interface NumberInputProps {
     onChange: (value: number) => void
 }
 
-export const NumberInput: Component<NumberInputProps> = (props) => 
-    <TextField id={props.id} size='small' variant='outlined' sx={{width: '14ch'}}
-        value={props.value}
+const isNumeric = (text: string) => /^[0-9]*([.][0-9]*)?$/.test(text)
+
+export const NumberInput: Component<NumberInputProps> = (props) => {
+    const [value, setValue] = createSignal(props.value.toString())
+
+
+    const onChange = (e: Event) => {
+        const input = e.target as HTMLInputElement
+        const newValue = input.value
+
+        if (!isNumeric(newValue))
+            return
+
+        const newNumber = Number.parseFloat(newValue)
+        const isValid = newValue !== '' && !Number.isNaN(newNumber)
+
+        setValue(newValue)
+        if (isValid)
+            props.onChange(newNumber)
+    }
+
+    const onBlur = () => {
+        setValue(props.value.toString())
+    }
+
+    return <TextField id={props.id} size='small' variant='outlined' sx={{ width: '14ch' }}
+        value={value()}
         disabled={props.disabled}
         label={props.label}
         type={props.integer ? 'number' : 'text'}
-        onChange={e => {
-            const value = Number.parseFloat(e.target.value)
-            if (!Number.isNaN(value))
-                props.onChange(value)
-        }}
+        onChange={e => onChange(e)}
+        onBlur={_ => onBlur()}
         InputProps={{
             endAdornment: props.units ? <InputAdornment position='end'>{props.units}</InputAdornment> : undefined,
-        }}/>
+        }} />
+}
